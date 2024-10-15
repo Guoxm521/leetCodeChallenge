28bc29a4092324569aa92bd7cc1ee8ae5686fad5
gxm <2925891606@qq.com>
2024-10-15 18:53:41 +0800

    feat: :sparkles: +1

--- 13.罗马数字转整数.js
+++ 13.罗马数字转整数.js
@@ -9,8 +9,32 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-  
+var romanToInt = function (s) {
+  let obj = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+  }
+  let value = 0
+  for (let index = 0; index < s.length; index++) {
+    const item = obj[s[index]];
+    if (index == 0) {
+      value += item
+    } else {
+      let left = obj[s[index - 1]];
+      if (left < item) {
+        value -= left
+        value -= left
+      }
+      value += item
+    }
+  }
+  return value
 };
+
 // @lc code=end
 
